2261bfe9575d0c65f4fbf0e0c7bf1570e6945284
RandomEngy <david.rickard@gmail.com>
2017-01-30 07:51:50 -0800

    Partial work on NSubstitute

---
+++ Sandbox/SandboxNSubstituteTestCases/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SandboxNSubstituteTestCases
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestInitialize]
+		public void TestInitialize()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
