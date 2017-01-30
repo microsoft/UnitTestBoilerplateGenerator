acb813f9ff11924d5dda551e5fe4b1fc2e4d63d0
RandomEngy <david.rickard@gmail.com>
2017-01-29 23:30:06 -0800

    Update unused BoilerplateSettings, attempting to work with MEF.

--- src/BoilerplateSettings.cs
+++ src/BoilerplateSettings.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.Settings;
+﻿using Microsoft.VisualStudio.Settings;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Settings;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace UnitTestBoilerplate
 {
-	// TODO: Migrate from StaticBoilerplateSettings to this
-	[PartCreationPolicy(CreationPolicy.Shared)]
-	[Export(typeof(IBoilerplateSettings))]
 	public class BoilerplateSettings : IBoilerplateSettings
 	{
 		private const string CollectionPath = "UnitTestBoilerplateGenerator";
@@ -25,19 +24,9 @@ namespace UnitTestBoilerplate
 		// Key is the solution path and value is the last used unit test project for the solution.
 		private readonly Dictionary<string, string> TestProjectsDictionary;
 
-		//[ImportingConstructor]
-		//public Encouragements(SVsServiceProvider vsServiceProvider)
-		//{
-		//	var shellSettingsManager = new ShellSettingsManager(vsServiceProvider);
-		//	writableSettingsStore = shellSettingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
-
-		//	LoadSettings();
-		//}
-
-		[ImportingConstructor]
-		public BoilerplateSettings(SVsServiceProvider vsServiceProvider)
+		public BoilerplateSettings()
 		{
-			SettingsManager settingsManager = new ShellSettingsManager(vsServiceProvider);
+			SettingsManager settingsManager = new ShellSettingsManager(ServiceProvider.GlobalProvider);
 			Store = settingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
 			Store.CreateCollection(CollectionPath);
 
@@ -75,6 +64,72 @@ namespace UnitTestBoilerplate
 			}
 
 			return null;
+		}
+
+		public string GetTemplate(MockFramework mockFramework, TemplateType templateType)
+		{
+			string templateSettingKey = GetTemplateSettingsKey(mockFramework, templateType);
+
+			if (Store.PropertyExists(CollectionPath, templateSettingKey))
+			{
+				return Store.GetString(CollectionPath, GetTemplateSettingsKey(mockFramework, templateType));
+			}
+
+			return GetDefaultTemplate(mockFramework, templateType);
+		}
+
+		public string GetDefaultTemplate(MockFramework mockFramework, TemplateType templateType)
+		{
+			// When looking for default templates, redirect "Unknown" to "Moq"
+			if (mockFramework == MockFramework.Unknown)
+			{
+				mockFramework = MockFramework.Moq;
+			}
+
+			using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"UnitTestBoilerplate.DefaultTemplates.{mockFramework}.{templateType}.txt"))
+			{
+				if (stream == null)
+				{
+					return string.Empty;
+				}
+
+				using (var reader = new StreamReader(stream))
+				{
+					return reader.ReadToEnd();
+				}
+			}
+		}
+
+		public void SetTemplate(MockFramework mockFramework, TemplateType templateType, string template)
+		{
+			Store.SetString(CollectionPath, GetTemplateSettingsKey(mockFramework, templateType), template);
+		}
+
+		public void SetTemplate(string mockFrameworkString, string templateTypeString, string template)
+		{
+			Store.SetString(CollectionPath, GetTemplateSettingsKey(mockFrameworkString, templateTypeString), template);
+		}
+
+		public void RevertTemplateToDefault(MockFramework mockFramework)
+		{
+			foreach (TemplateType templateType in Enum.GetValues(typeof(TemplateType)))
+			{
+				string templateSettingKey = GetTemplateSettingsKey(mockFramework, templateType);
+				if (Store.PropertyExists(CollectionPath, templateSettingKey))
+				{
+					Store.DeleteProperty(CollectionPath, templateSettingKey);
+				}
+			}
+		}
+
+		private static string GetTemplateSettingsKey(MockFramework mockFramework, TemplateType templateType)
+		{
+			return GetTemplateSettingsKey(mockFramework.ToString(), templateType.ToString());
+		}
+
+		private static string GetTemplateSettingsKey(string mockFrameworkString, string templateTypeString)
+		{
+			return $"Template_{mockFrameworkString}_{templateTypeString}";
 		}
 	}
 }
