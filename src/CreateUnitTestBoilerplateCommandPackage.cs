5b499801cb80c88276789dda99839099850633e8
RandomEngy <david.rickard@gmail.com>
2018-07-05 08:09:32 -0700

    Enabled background loading for the extension.

--- src/CreateUnitTestBoilerplateCommandPackage.cs
+++ src/CreateUnitTestBoilerplateCommandPackage.cs
@@ -41,7 +41,7 @@ namespace UnitTestBoilerplate
 	/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
 	/// </para>
 	/// </remarks>
-	[PackageRegistration(UseManagedResourcesOnly = true)]
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
 	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
 	[ProvideMenuResource("Menus.ctmenu", 1)]
 	[ProvideAutoLoad(Microsoft.VisualStudio.Shell.Interop.UIContextGuids.SolutionExists)]
